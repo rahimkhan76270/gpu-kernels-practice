167be0a638d9d4ea886b3f757a8cef7a1abef05b
Rahim Khan <36726823+rahimkhan76270@users.noreply.github.com>
2025-06-23 10:59:01 +0530

    Create sigmoid.cu

---
+++ sigmoid.cu
@@ -0,0 +1,21 @@
+#include <cuda_runtime.h>
+#include<cmath>
+
+__global__ void SigmoidKernel(const float* input, float* output, size_t n, size_t m, float alpha)
+{
+    int row=blockIdx.y*blockDim.y+threadIdx.y;
+    int col=blockIdx.x*blockDim.x+threadIdx.x;
+    int idx=row*n+col;
+    if(row<m && col<n)
+    {
+        output[idx]=1/(1+exp(-input[idx]));
+    }
+}
+
+// Note: input, output are all device pointers to float32 arrays
+extern "C" void solution(const float* input, float* output, size_t n, size_t m, float alpha) {    
+    dim3 threads_per_block(16,16);
+    dim3 num_blocks((n+threads_per_block.x-1)/threads_per_block.x,(m+threads_per_block.y-1)/threads_per_block.y);
+    SigmoidKernel<<<num_blocks,threads_per_block>>>(input,output,n,m,alpha);
+    
+}
